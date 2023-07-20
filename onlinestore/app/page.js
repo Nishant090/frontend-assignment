b893f085a9c5cdaab3a6662b17d61983f8fd0876
Nishant <nishantkhatiwada6@gmail.com>
2023-07-20 18:00:34 +0545

    updating home page

--- onlinestore/app/page.js
+++ onlinestore/app/page.js
@@ -1,9 +1,26 @@
-import Image from 'next/image'
+import React from 'react'
 
-export default function Home() {
-  return (<>
-  <h1>Home</h1>
-  </>
-    
+
+const page = () => {
+  return (
+    <>
+    <section class="text-gray-600 body-font">
+  <div class="container pt-10 pb-24 mx-auto">
+    <div class="flex flex-wrap -mb-10 text-center">
+      <div class="sm:w-1/2 mb-10 md:w-full ">
+        <div class="rounded-lg h-96">
+        <h1 class="absolute text-white font-medium text-5xl pl-10 pt-28 md:text-8xl md:pl-[29rem]">Online Store</h1>
+          <h1 class="absolute text-white font-medium text-xl pl-[4.5rem] pt-[10rem] md:text-3xl md:pt-[14rem] md:pl-[36rem]">You Seek || We Provide</h1>
+          <img alt="background" class="object-cover object-center h-full w-full" src='/images/background.jpg'/>
+         
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+      
+    </>
   )
 }
+
+export default page
